10edebbf747ed3e0fde37aaa2728428a1a4f3fc5
pgcudahy <pgcudahy@gmail.com>
2020-02-15 15:05:25 +0200

    getting standalone package working

--- R/data.R
+++ R/data.R
@@ -3,6 +3,8 @@
 #' @format A data frame with 2 rows and 3 variables:
 #' \describe{
 #'   \item{lesson_file}{relative location of the lesson file}
+#'   \item{lesson_base}{basename of lesson file}
+#'   \item{lesson_directory}{path of lesson file from project root}
 #'   \item{number_koans}{number of koans in the lesson}
 #'   \item{cumulative_koans}{number of cumulative koans through that lesson}
 #'   \item{test_file}{relative location of the test file}
--- data-raw/koans.R
+++ data-raw/koans.R
@@ -1,17 +1,19 @@
 ## code to prepare `koans` dataset goes here
-koans <- list(
-    "R/koans/about-expectations.R",
-    "R/koans/about-binding-basics.R",
-    "R/koans/about-copy-on-modify.R")
+koans <- c(
+    "inst/koans/about-expectations.R",
+    "inst/koans/about-binding-basics.R",
+    "inst/koans/about-copy-on-modify.R")
 
-koans_df <- dplyr::tibble(lesson_file = unlist(koans)) %>%
+koans_df <- dplyr::tibble(lesson_file = koans) %>%
+    dplyr::mutate(lesson_base = basename(lesson_file)) %>%
+    dplyr::mutate(lesson_directory = dirname(lesson_file)) %>%
     dplyr::mutate(number_koans = purrr::map_int(lesson_file, function(x)
         getParseData(parse(file = x)) %>%
         dplyr::filter(text == "test_that") %>%
         nrow)) %>%
     dplyr::mutate(cumulative_koans = purrr::accumulate(number_koans, `+`)) %>%
     dplyr::mutate(test_file = lesson_file %>%
-        stringr::str_replace("R/koans/", "tests/testthat/") %>%
+        stringr::str_replace("inst/koans/", "tests/testthat/") %>%
         stringr::str_replace("about", "test-about"))
 
 usethis::use_data(koans_df, overwrite = T, internal = TRUE)
